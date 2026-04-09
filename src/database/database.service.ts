4dc229ad852b3d4c777290712aec3609d0caaa33
KRW <kyungrok.won@gmail.com>
2026-04-08 21:08:03 -0500

    fix: handle database error

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,8 +1,17 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma/client'
+import { PrismaClient } from '../generated/prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon';
 
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
+    constructor(){
+        const adapter = new PrismaNeon({
+            connectionString: process.env.DATABASE_URL!
+        });
+        super({
+            adapter: adapter
+        })
+    }
     async onModuleInit() {
         await this.$connect
     }
