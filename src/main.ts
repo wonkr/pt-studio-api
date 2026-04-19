8abd81fea0596f3e42a7a8b0943e29193cb24aa6
KRW <kyungrok.won@gmail.com>
2026-04-18 21:33:49 -0500

    Update: apply `helmet`

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { PrismaExceptionFilter } from './filters/prisma-exception.filter';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
+  const app = await NestFactory.create(AppModule)
+  app.use(cookieParser())
   app.useGlobalFilters(new PrismaExceptionFilter())
+  app.use(helmet())
   app.setGlobalPrefix('api')
   await app.listen(process.env.PORT ?? 3000);
 }
