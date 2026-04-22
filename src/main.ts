dda158543f87f05ceb3e333a45ec1a0104f1c7e0
KRW <kyungrok.won@gmail.com>
2026-04-21 23:28:56 -0500

    Update: global validation setting

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,13 @@ async function bootstrap() {
     origin: 'http://localhost:5173',
     credentials: true,
   })
-  app.useGlobalPipes(new ValidationPipe({transform: true}))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  )
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
